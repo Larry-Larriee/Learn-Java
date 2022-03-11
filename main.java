0e8f268994e05ac034e4ae4c0d8d29c7da7e8c39
Larry Le <lle114@student.hbuhsd.edu>
2022-03-10 22:16:33 -0800

    Hello World

---
+++ main.java
@@ -0,0 +1,8 @@
+// A public class goes hand in hand with the file name
+public class main {
+    public static void main (String[] args){
+        
+        Integer test = 13;
+        System.out.println(test);
+    }
+}
