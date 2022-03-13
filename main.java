d52a113214253f4372e6b7c985de781abc7f5af7
Larry Le <lle114@student.hbuhsd.edu>
2022-03-13 16:02:48 -0700

    Test

--- main.java
+++ main.java
@@ -1,8 +1,11 @@
 // A public class goes hand in hand with the file name
 public class main {
+
+    // Create an array of string parameters
     public static void main (String[] args){
         
         Integer test = 13;
         System.out.println(test);
     }
+
 }
